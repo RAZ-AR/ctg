d04003d8aedda0cadf2d6de24bfca713f37d1f2a
RAZ-AR <razmikovich@gmail.com>
2026-07-06 13:24:58 +0200

    Connect API routes to Supabase

--- api/menu.js
+++ api/menu.js
@@ -1,9 +1,9 @@
-import { getMenuResponse } from "./_lib/mockState.js";
 import { json } from "./_lib/response.js";
+import { getMenu } from "./_lib/supabase.js";
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method === "OPTIONS") return json(res, 200, { ok: true });
   if (req.method !== "GET") return json(res, 405, { error: "Method not allowed" });
 
-  return json(res, 200, getMenuResponse());
+  return json(res, 200, await getMenu(req.query?.lang));
 }
